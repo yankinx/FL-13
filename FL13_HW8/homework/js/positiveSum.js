9cd9867285768a43437085bb0c196c16ffbdc16e
Yan Parasyuk <46303327+yankinz@users.noreply.github.com>
2020-05-01 19:43:26 +0300

    Add files via upload

--- FL13_HW8/homework/js/positiveSum.js
+++ FL13_HW8/homework/js/positiveSum.js
@@ -1,9 +1,9 @@
-function positiveSum() {
+function positiveSum(array) {
     let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i] > 0 ? arguments[i] : 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i] > 0 ? array[i] : 0;
     }
     return sum;
 }
 
-console.log(positiveSum(5, -2, 4));
+console.log(positiveSum([5, -2, 4, 5]));
